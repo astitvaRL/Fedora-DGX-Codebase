b50c8a1b0adcba2c9a90edadddce61629d262fa0
astitvaRL <astitva@meta.com>
2024-09-05 12:12:37 -0700

    Updated

--- SEGMENTATION/SAM_finetuning/utils/dataset.py
+++ SEGMENTATION/SAM_finetuning/utils/dataset.py
@@ -112,3 +112,52 @@ class Dataset_hybrid(Dataset):
             return input_image_tensor, torch.tensor(img_embed).float(), torch.tensor(gt2D[None, :,:]).long(), torch.tensor(binmask[None, :,:]).float(), torch.from_numpy(np.array([min(Ys),min(Xs),max(Ys),max(Xs)])).float()
         else:
             return input_image_tensor, None, torch.tensor(gt2D[None, :,:]).long(), torch.tensor(binmask[None, :,:]).float(), torch.from_numpy(np.array([min(Ys),min(Xs),max(Ys),max(Xs)])).float()
+
+
+
+# dataset definition for hybrid training
+class Dataset_body(Dataset): 
+    def __init__(self, sam_model, data_root, img_dir_name, img_embed_dir_name, label_id_dir_name, mode='train', device='cuda', return_embeddings=True):
+        self.sam_model = sam_model
+        self.mode = mode
+        self.device = device
+        self.return_embeddings = return_embeddings
+        self.data_root = data_root
+        self.image_dir_name = img_dir_name
+        self.label_id_dir_name = label_id_dir_name
+        self.img_embed_dir_name = img_embed_dir_name
+        self.files = sorted(os.listdir(join(self.data_root, self.img_embed_dir_name)))[:-100]
+        if self.mode == 'test':
+            self.files = sorted(os.listdir(join(self.data_root, self.img_embed_dir_name)))[-100:]
+        print(f"{mode} dataset : {len(self.files)} --> {self.files[0]} -- {self.files[-1]}") 
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, index):
+        image = io.imread(join(self.data_root, self.image_dir_name, self.files[index][:-4]+'.png'))
+        sam_transform = ResizeLongestSide(self.sam_model.image_encoder.img_size)
+        resize_img = sam_transform.apply_image(image)
+        resize_img_tensor = torch.as_tensor(resize_img.transpose(2, 0, 1)).to(self.device)
+        input_image_tensor = self.sam_model.preprocess(resize_img_tensor[None,:,:,:]) # (1, 3, 1024, 1024)
+        input_image_tensor = input_image_tensor.squeeze(0)
+
+
+        img_embed = np.load(join(self.data_root, self.img_embed_dir_name, self.files[index][:-4]+'.npy')) #validate this logic in case filename convention changes
+        gt2D = io.imread(join(self.data_root, self.label_id_dir_name, self.files[index][:-6]+'.png'))
+
+
+        # binary mask from GT, at inference can be replaced by an off-the-shelf model prediction (e.g. SAM)
+        binmask = gt2D>0
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+        binmask = cv2.morphologyEx(binmask.astype('uint8'),cv2.MORPH_CLOSE,kernel)
+
+        # for bounding box
+        Xs = np.where(binmask>0)[0]
+        Ys = np.where(binmask>0)[1]
+        
+        # convert img embedding, gt, mask, bounding box to torch tensor
+        if self.return_embeddings:
+            return input_image_tensor, torch.tensor(img_embed).float(), torch.tensor(gt2D[None, :,:]).long(), torch.tensor(binmask[None, :,:]).float(), torch.from_numpy(np.array([min(Ys),min(Xs),max(Ys),max(Xs)])).float()
+        else:
+            return input_image_tensor, None, torch.tensor(gt2D[None, :,:]).long(), torch.tensor(binmask[None, :,:]).float(), torch.from_numpy(np.array([min(Ys),min(Xs),max(Ys),max(Xs)])).float()
