ab3cfe5926228f97026f1e39c60f47326825f588
Astitva Srivastava <astitva@ash6-hpccdgx-node0105.thefacebook.com>
2024-09-07 16:10:14 -0700

    Updated

--- SEGMENTATION/SAM_finetuning/utils/dataset.py
+++ SEGMENTATION/SAM_finetuning/utils/dataset.py
@@ -115,3 +115,51 @@ class Dataset_precomputed_body(Dataset):
         
         # convert img embedding, gt, mask, bounding box to torch tensor
         return torch.tensor(img_embed).float(), torch.tensor(gt2D_labels[None, :,:]).long(), torch.tensor(binmask[None, :,:]).float(), torch.from_numpy(np.array([min(Ys),min(Xs),max(Ys),max(Xs)])).float()
+
+
+
+# dataset definition for only body (no facial details) real data
+class Dataset_precomputed_body_real(Dataset): 
+    def __init__(self, data_root, labels_definition_file_path, img_dir_name, img_embed_dir_name, label_id_dir_name, mode='train', device='cuda'):
+        self.num_classes = 18
+        self.semantics = SemanticSegmentation(labels_definition_path=labels_definition_file_path, num_classes=self.num_classes)
+        self.mode = mode
+        self.device = device
+        self.data_root = data_root
+        self.image_dir_name = img_dir_name
+        self.label_id_dir_name = label_id_dir_name
+        self.img_embed_dir_name = img_embed_dir_name
+        self.files = sorted(os.listdir(join(self.data_root, self.label_id_dir_name)))
+        self.files = [f for f in self.files if f.endswith('_1024.png')]
+        if self.mode == 'train':
+            self.files = self.files[:-100]
+        if self.mode == 'test':
+            self.files = self.files[-100:]
+        print(f"{mode} dataset : {len(self.files)} --> {self.files[0]} -- {self.files[-1]}") 
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, index):
+
+        embedding_name = f"{self.files[index].split('_')[0]}.npy"
+        img_embed = np.load(join(self.data_root, self.img_embed_dir_name, embedding_name)) #validate this logic in case filename convention changes
+        
+        # load GT semantic segmentation map
+        gt2D = io.imread(join(self.data_root, self.label_id_dir_name, self.files[index]))
+
+        # binary mask from GT, at inference can be replaced by an off-the-shelf model prediction (e.g. SAM)
+        r,g,b = cv2.split(gt2D)
+        binmask = ~((r==0) & (g==0) & (b==0))
+        binmask = np.uint8(binmask)
+
+        # colors to labels
+        gt2D_labels = self.semantics.colors_to_labels(gt2D)
+        # gt2D_labels_vis = self.semantics.labels_to_colors(gt2D_labels)
+
+        # for bounding box
+        Xs = np.where(binmask>0)[0]
+        Ys = np.where(binmask>0)[1]
+
+        # convert img embedding, gt, mask, bounding box to torch tensor
+        return torch.tensor(img_embed).float(), torch.tensor(gt2D_labels[None, :,:]).long(), torch.tensor(binmask[None, :,:]).float(), torch.from_numpy(np.array([min(Ys),min(Xs),max(Ys),max(Xs)])).float()
