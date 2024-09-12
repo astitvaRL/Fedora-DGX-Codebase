19819f4670cb255fc47b4c3a9b40c3fabc742268
Astitva Srivastava <astitva@ash6-hpccdgx-node0107.thefacebook.com>
2024-09-11 17:20:26 -0700

    Added MULTI-GPU Support

--- SEGMENTATION/SAM_finetuning/parallel_finetune_decoder_SAM_body_only.py
+++ SEGMENTATION/SAM_finetuning/parallel_finetune_decoder_SAM_body_only.py
@@ -118,7 +118,7 @@ if __name__ == '__main__':
         test_dataset.init_cache()
 
     # create dataloader
-    train_dataloader = DataLoader(train_dataset, batch_size=160, shuffle=True, num_workers=0, drop_last=True)
+    train_dataloader = DataLoader(train_dataset, batch_size=80, shuffle=True, num_workers=0, drop_last=True)
     test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=0, drop_last=True)
 
     # training config
