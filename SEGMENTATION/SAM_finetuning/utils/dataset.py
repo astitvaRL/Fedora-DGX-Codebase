20ac88954e7ba7803783196fe4b5b5cde891b5a8
Astitva Srivastava <astitva@ash6-hpccdgx-node0108.thefacebook.com>
2024-10-04 17:23:22 -0700

    Updated

--- SEGMENTATION/SAM_finetuning/utils/dataset.py
+++ SEGMENTATION/SAM_finetuning/utils/dataset.py
@@ -13,7 +13,7 @@ from scipy.spatial import cKDTree
 join = os.path.join
 
 from segment_anything.utils.transforms import ResizeLongestSide
-from .SemanticSegmentation import SemanticSegmentationNoFace, SemanticSegmentationFace, SemanticSegmentationCoarse
+from .SemanticSegmentation import SemanticSegmentationNoFace, SemanticSegmentationFace, SemanticSegmentationCoarse, SemanticSegmentationAll
 
 
 ###########################################################################################################################################
@@ -346,6 +346,102 @@ class DrawingsDatasetC2F(Dataset):
         print(f"Initializing {self.mode} cache...")
         cache_length = len(self.files) # number of samples you want to cache
         data_dims = (5, 1024, 1024) # shape of data (not including batch)
+        shared_array_base = mp.Array(ctypes.c_float, cache_length * data_dims[0] * data_dims[1] * data_dims[2])
+        shared_array = np.ctypeslib.as_array(shared_array_base.get_obj())
+        shared_array = shared_array.reshape(cache_length, *data_dims)
+        self.cache = torch.from_numpy(shared_array)
+        self.cache *=0
+        print(f"{self.mode} dataset : {len(self.files)} --> {self.files[0]} -- {self.files[-1]}")
+    
+    def save_cache(self, path):
+        torch.save(self.cache, path)
+
+    def load_cache(self, path):
+        self.cache = torch.load(path)
+
+###########################################################################################################################################
+### ----------------------------------------------------------------------------------------------------------------------------------- ###
+###########################################################################################################################################
+
+
+# dataset definition for all classes, coarse-to-fine
+class DrawingsDatasetC2FAll(Dataset): 
+    def __init__(self, sam_model, data_root, labels_definition_file_path, img_dir_name, label_id_dir_name, mode='train', device='cuda', num_test_samples=100):
+        self.sam_model = sam_model
+        self.num_classes_coarse = 5
+        self.num_classes_fine = 18
+        self.num_classes_face = 11
+        self.num_classes_all = 27
+        self.semantics_coarse = SemanticSegmentationCoarse(labels_definition_path=labels_definition_file_path, num_classes=self.num_classes_coarse, exclude_neck=True)
+        self.semantics_fine = SemanticSegmentationNoFace(labels_definition_path=labels_definition_file_path, num_classes=self.num_classes_fine)
+        self.semantics_face = SemanticSegmentationFace(labels_definition_path=labels_definition_file_path, num_classes=self.num_classes_face)
+        self.semantics_all = SemanticSegmentationAll(labels_definition_path=labels_definition_file_path, num_classes=self.num_classes_all)
+        self.compute_bbox=False # slower data loading if True
+        self.mode = mode
+        self.cache_available=False
+        self.cache = None
+        self.device = device
+        self.data_root = data_root
+        self.image_dir_name = img_dir_name
+        self.label_id_dir_name = label_id_dir_name
+        self.num_test_samples = num_test_samples
+        self.files = sorted(os.listdir(join(self.data_root, self.label_id_dir_name)))[:-self.num_test_samples]
+        if self.mode == 'test':
+            self.files = sorted(os.listdir(join(self.data_root, self.label_id_dir_name)))[-self.num_test_samples:]
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, index):
+        input_image_tensor = self.cache[index][:3,:,:]
+        if not self.cache_available: 
+            image_name = f"{self.files[index].split('_')[0]}.png"
+            # populate cache entry during first-time access
+            image = cv2.imread(join(self.data_root, self.image_dir_name, image_name))
+            image = cv2.resize(image, (1024,1024), interpolation=cv2.INTER_LINEAR)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            sam_transform = ResizeLongestSide(self.sam_model.image_encoder.img_size)
+            resize_img = sam_transform.apply_image(image)
+            resize_img_tensor = torch.as_tensor(resize_img.transpose(2, 0, 1)).to(self.device)
+            input_image_tensor = self.sam_model.preprocess(resize_img_tensor[None,:,:,:]) # (1, 3, 1024, 1024)
+            input_image_tensor = input_image_tensor.squeeze(0)
+            self.cache[index][:3,:,:] = input_image_tensor
+
+        gt2D_labels_coarse = self.cache[index][3:4,:,:]
+        gt2D_labels_fine = self.cache[index][4:5,:,:]
+        gt2D_labels_face = self.cache[index][5:6,:,:]
+        gt2D_labels_all = self.cache[index][6:,:,:]
+        if not self.cache_available: 
+            # populate cache entry during first-time access
+            gt2D = cv2.imread(join(self.data_root, self.label_id_dir_name, self.files[index]))
+            gt2D = cv2.resize(gt2D, (1024,1024), interpolation=cv2.INTER_NEAREST)
+            gt2D = cv2.cvtColor(gt2D, cv2.COLOR_BGR2RGB)
+            gt2D_labels_coarse = self.semantics_coarse.colors_to_labels(gt2D)
+            gt2D_labels_fine = self.semantics_fine.colors_to_labels(gt2D)
+            gt2D_labels_face = self.semantics_face.colors_to_labels(gt2D)
+            gt2D_labels_all = self.semantics_all.colors_to_labels(gt2D)
+            gt2D_labels_coarse = torch.tensor(gt2D_labels_coarse[None, :,:]).float()
+            gt2D_labels_fine = torch.tensor(gt2D_labels_fine[None, :,:]).float()
+            gt2D_labels_face = torch.tensor(gt2D_labels_face[None, :,:]).float()
+            gt2D_labels_all = torch.tensor(gt2D_labels_all[None, :,:]).float()
+            # vis_gt2D_labels = self.semantics.labels_to_colors(gt2D_labels)
+            self.cache[index][3:4,:,:] = gt2D_labels_coarse
+            self.cache[index][4:5,:,:] = gt2D_labels_fine
+            self.cache[index][5:6,:,:] = gt2D_labels_face
+            self.cache[index][6:,:,:] = gt2D_labels_all
+
+        
+        if self.semantics_coarse.exclude_neck:
+            neck_mask = gt2D_labels_coarse==5
+            gt2D_labels_coarse[neck_mask] = 4 #merge with torso
+
+        # convert image, gt, mask, bounding box to torch tensor
+        return input_image_tensor, gt2D_labels_coarse.long(), gt2D_labels_fine.long(),  gt2D_labels_face.long(),  gt2D_labels_all.long()
+
+    def init_cache(self):
+        print(f"Initializing {self.mode} cache...")
+        cache_length = len(self.files) # number of samples you want to cache
+        data_dims = (7, 1024, 1024) # shape of data (not including batch)
         shared_array_base = mp.Array(ctypes.c_float, cache_length * data_dims[0] * data_dims[1] * data_dims[2])
         shared_array = np.ctypeslib.as_array(shared_array_base.get_obj())
         shared_array = shared_array.reshape(cache_length, *data_dims)
