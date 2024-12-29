5b3ca500685109de5873551ab9acc1da63336543
Astitva Srivastava <astitva@ash6-hpccdgx-node0106.thefacebook.com>
2024-12-28 18:52:10 -0800

    SEAN Asset Generation

--- GENERATION/PresetGeneration/deform_presets_mouth.py
+++ GENERATION/PresetGeneration/deform_presets_mouth.py
@@ -26,8 +26,11 @@ from utils.SegmentDrawings import SAM_face
 
 join = os.path.join
 
-def to_bgr(img):
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+def bgr_conversion(img):
+    if img.shape[-1] == 4:
+        img = cv2.cvtColor(img, cv2.COLOR_RGBA2BGRA)
+    else:
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
     return img
 
 
@@ -46,10 +49,6 @@ shape_ids = preset_config.config["shape_ids"]
 output_root = f"/mnt/users_scratch/astitva/WORKSPACE/Fedora-DGX-Codebase/GENERATION/PresetGeneration/OUTPUT/DEFORMED_PRESETS/{preset_class}"
 
 os.makedirs(output_root, exist_ok=True)
-
-# load stylization model
-# control_stylization = ControllableStylization()
-# generation_model = get_model()
 
 # load inpainting model
 inpainting_model = SimpleLama()
@@ -71,11 +70,6 @@ labels = sorted(os.listdir(join(data_root, label_id_dir_name)))[start:end]
 
 # iterate over images
 for label_name in tqdm(labels):
-    # label_name = "sample_seg.png"
-    # img_name = "sample_bizzare.png"
-    # print(label_name)
-    # if not label_name.startswith('1525827019134c6f817dc63e83caabbe'):
-    #     continue
 
     condition = label_name.startswith('07ae7e78bf78421cb2219c236d156cbd') or label_name.startswith('0a5b805185614f839b5f015b650970dc') or label_name.startswith('07cdc5cab5af41e2a102e5453678ca72') or label_name.startswith('07a23dcc43ea436ebf6b75b04c7611d4') or label_name.startswith('07a73dddd01848a19eb97839500a2843') or label_name.startswith('07aba8228cb54faf9f6e4e6cab561331') or label_name.startswith('07ad6ccc1ac34288ab7c4f0a013ba3c3') or label_name.startswith('07aedcb335a04981a016c0c7efed77ba') or label_name.startswith('07af86863ff04f3abc0e0442cdb882b7') or label_name.startswith('07b1a55d68b9425caccb1aadcc58379a') or label_name.startswith('07b9b86ec22e48e1807785b2cd64cb76') or label_name.startswith('07b6c37d7a6944ee98f544d633defeeb') or label_name.startswith('07b8bf4a421744c9b7f985cf6e8fe544') or label_name.startswith('07b97debed234daaa04313b000637b81') or label_name.startswith('07babac076024ce7a89b72e93d16cc99') or label_name.startswith('07c4c1c55b5b4098bf8f5b96defd6d2c') or label_name.startswith('07c7e9cc8d364378912accf8b9c5eb57')
     if not condition:
@@ -94,16 +88,15 @@ for label_name in tqdm(labels):
     # load images
     img_name = f"{label_name.split('_')[0]}.png"
     img_full = cv2.imread(join(data_root, image_dir_name, img_name))
-    # img_full = cv2.imread(img_name)
     w_orig, h_orig = img_full.shape[:2]
     img_full = cv2.resize(img_full, (1024, 1024))
-    img_full = cv2.cvtColor(img_full, cv2.COLOR_BGR2RGB)
+    img_full = bgr_conversion(img_full)
     label_full = cv2.imread(join(data_root, label_id_dir_name, label_name))
-    # label_full = cv2.imread(label_name)
-    label_full = cv2.cvtColor(label_full, cv2.COLOR_BGR2RGB)
+    label_full = bgr_conversion(label_full)
     label_full = cv2.resize(label_full, (1024, 1024), interpolation=cv2.INTER_NEAREST)
     label_id = semantics.colors_to_labels(label_full)
     label_face_id = semantics_face.colors_to_labels(label_full)
+   
     # inpainting
     kernel = np.ones((5, 5), np.uint8)
     inpainting_mask = label_id == 0  # default background
@@ -115,12 +108,8 @@ for label_name in tqdm(labels):
         255 * inpainting_mask.astype("uint8"), kernel, iterations=3
     )
     base_image = inpainting_model(Image.fromarray(img_full.copy()), inpainting_mask)
+   
     # extract face region
-    # roi = (label_id == 2) | (label_id == 3) | (label_id == 4) | (label_id == 5) | (label_id == 6) | (label_id == 12) | (label_id == 13) | (label_id == 17) | (label_id == 18) | (label_id == 22) | (label_id == 23)
-    # roi = (label_id == 2) | (label_id == 3) | (label_id == 4) | (label_id == 5) | (label_id == 6) | (label_id == 13) | (label_id == 17) | (label_id == 22) | (label_id == 23)
-    # roi = (label_face_id == 2) | (label_face_id == 7) | (label_face_id == 10)   
-    # mouth_region = roi.copy() 
-    # preset cropping window
     roi = label_face_id>0
     Xs, Ys = np.where(roi)
     if len(Xs)==0 or len(Ys)==0: continue
@@ -145,6 +134,7 @@ for label_name in tqdm(labels):
     base_image_cropped = np.array(base_image)[x_min:x_max, y_min:y_max]
     h_crop, w_crop, _ = img_cropped.shape
 
+    roi_cropped = cv2.resize(roi_cropped.astype('uint8'), (1024, 1024), interpolation=cv2.INTER_NEAREST)
     img_cropped = cv2.resize(img_cropped, (1024, 1024))
     base_image_cropped = cv2.resize(base_image_cropped, (1024, 1024))
     label_id = cv2.resize(label_id, (1024, 1024), interpolation=cv2.INTER_NEAREST)
@@ -152,14 +142,15 @@ for label_name in tqdm(labels):
 
     dims = {'w_crop':w_crop,'h_crop':h_crop, 'w_orig':w_orig, 'h_orig':h_orig}
     np.savez(f'{output_dir_metadata}/dims.npz',**dims)
-    cv2.imwrite(f'{output_dir_metadata}/original.png', to_bgr(img_full))
-    cv2.imwrite(f'{output_dir_metadata}/original_image/original.png', to_bgr(img_cropped))
-    cv2.imwrite(f'{output_dir_metadata}/inpainted.png', to_bgr(np.array(base_image)))
-    cv2.imwrite(f'{output_dir_metadata}/inpainted_face.png', to_bgr(base_image_cropped))
-    cv2.imwrite(f'{output_dir_metadata}/segmap.png', cv2.cvtColor(label_full, cv2.COLOR_BGR2RGB))
+    cv2.imwrite(f'{output_dir_metadata}/original.png', bgr_conversion(img_full))
+    cv2.imwrite(f'{output_dir_metadata}/original_image/original.png', bgr_conversion(img_cropped))
+    cv2.imwrite(f'{output_dir_metadata}/inpainted.png', bgr_conversion(np.array(base_image)))
+    cv2.imwrite(f'{output_dir_metadata}/inpainted_face.png', bgr_conversion(base_image_cropped))
+    cv2.imwrite(f'{output_dir_metadata}/segmap.png', bgr_conversion(label_full))
     cv2.imwrite(f'{output_dir_metadata}/label.png', label_id)
     cv2.imwrite(f'{output_dir_metadata}/original_label/original.png', label_face_id)
 
+    deformed_masks_all = []
     # iterate over presets for deformation
     for preset_idx in tqdm(range(len(shape_ids))):
         # load preset
@@ -172,18 +163,9 @@ for label_name in tqdm(labels):
         # get preset shape
         tps_output = None
         if preset_class == "mouth" or preset_class == "mouth_talk" or preset_class == "arpabets":
-            # tps_output = tps_warp_box_mouth(
-            #     image=img_cropped,
-            #     label_id=label_id,
-            #     label_type=preset_class,
-            #     preset_shape=preset_image,
-            #     shape_id=shape_id,
-            # )
             tps_output = tps_warp_preset_mouth(img_cropped, label_id, preset_image)
 
-            # 
-            # 
-            # 
+
         elif preset_class == "eyes":
             tps_output = tps_warp_preset_eyes(
                 label_id=label_face_id, label_type=preset_class, preset_shape=preset_image
@@ -192,26 +174,13 @@ for label_name in tqdm(labels):
             print("Skipping...")
             continue
 
-        # deformed, _, mouth_pose = tps_output
-        # mouth_pose = 'preset'
-
-        # resize crops to 1024x1024
-        img_cropped = cv2.resize(img_cropped, (1024, 1024))
-        base_image_cropped = cv2.resize(base_image_cropped, (1024, 1024))
-        label_face_id = cv2.resize(label_face_id, (1024, 1024), interpolation=cv2.INTER_NEAREST)
-        # deformed = cv2.resize(deformed, (1024, 1024), interpolation=cv2.INTER_NEAREST)
-        roi_cropped = cv2.resize(roi_cropped.astype('uint8'), (1024, 1024), interpolation=cv2.INTER_NEAREST)
-
         # mask from tps  deformation
-        # deformed_mask = deformed[:, :, 3] == 255
         deformed_mask = tps_output[1]
         deformed_preset = tps_output[3]
         # if shape_id=='18' or shape_id=='19': #remove the green area around the preset (which was used to enable salient point detection on preset)
         deformed_mask[deformed_preset[:,:,1]==255]=0
         teeth = (deformed_preset[:,:,0]>0) & (deformed_preset[:,:,0]<200)
         tongue = deformed_preset[:,:,0]>200
-
-        # refined_binmask = refiner.refine(deformed[:,:,:3].astype('uint8'), deformed_mask.astype('uint8')*255, fast=False, L=900)
 
         # place preset shape over inpainted image
         base_image_np = np.array(base_image_cropped)
@@ -227,7 +196,6 @@ for label_name in tqdm(labels):
         # deformed_mask = cv2.GaussianBlur(deformed_mask.astype('uint8')*255, blur_kernel, 0)
         deformed_mask = deformed_mask>0
 
-
         # prepare conditioning image
         label_id_cropped[deformed_mask] = 3
         label_id_cropped[teeth] = 17
@@ -236,8 +204,12 @@ for label_name in tqdm(labels):
 
         cond_face_labels = semantics_face.colors_to_labels(cond_image)
 
-        cv2.imwrite(f'{output_dir_images}/{shape_id}.png', to_bgr(img_cropped))
+        cv2.imwrite(f'{output_dir_images}/{shape_id}.png', bgr_conversion(img_cropped))
         cv2.imwrite(f'{output_dir_labels}/{shape_id}.png', cond_face_labels)
+
+        # include every part in final binary mask
+        deformed_mask = (deformed_mask>0) | (teeth>0) | (tongue>0)
+        deformed_masks_all.append(deformed_mask)
 
     # generating via SEAN
     sean_outdir = join(output_root, label_name.split(".")[0], 'sean_output')
@@ -250,113 +222,46 @@ for label_name in tqdm(labels):
     # run the script
     os.system('bash seg2image.sh')
 
-    # # compositing
-    # for preset_idx in tqdm(range(len(shape_ids))):
-    #     cond_image[cond_image.sum(2)==0] = [255,255,255]
-    #     # cond_image[deformed_mask] = deformed[:, :, :3][deformed_mask]
-    #     cond_image = cond_image.astype("uint8")
+    # compositing
+    asset_outdir = join(output_root, label_name.split(".")[0], 'assets')
+    composite_outdir = join(output_root, label_name.split(".")[0], 'assets_composited')
+    os.makedirs(asset_outdir, exist_ok=True)
+    os.makedirs(composite_outdir, exist_ok=True)
+    for preset_idx in tqdm(range(len(shape_ids))):
+        shape_id = shape_ids[preset_idx]
+        deformed_mask = deformed_masks_all[preset_idx]
         
-    #     # stylization
-    #     # target_prompt = f"zoomed in mouth of a cartoon character {preset_prompts[preset_idx]}"
-    #     # target_prompt = f"a solid filled mouth of an animated handrawn character"
-    #     # target_prompt = f"a solid filled mouth of an animated handrawn character with a tongue"
+        generated = cv2.imread(f'{sean_outdir}/{preset_idx}.png')
+        generated = bgr_conversion(generated)
 
-    #     ref_image = img_cropped.copy()
+        deformed_mask_im = cv2.dilate(deformed_mask.astype('uint8')*255, (23,23))
+        deformed_mask_im = refiner.refine(generated, deformed_mask_im, fast=False, L=900)
+        deformed_mask_im = np.repeat(deformed_mask_im[..., np.newaxis], 3, axis=2)
+        deformed_mask_im = cv2.blur(deformed_mask_im, (23, 23))
+        deformed_mask_im = deformed_mask_im.astype("float32")/255
+        final_image = base_image_np * (1 - deformed_mask_im) + generated * deformed_mask_im
+        final_image = final_image.astype("uint8")
+        asset_image = np.concatenate([generated,255*deformed_mask_im[:,:,0:1]],-1).astype('uint8')
 
-    #     # generated = np.zeros_like(ref_image)
-    #     # generated, conditioning = control_stylization.generate(
-    #     #     ref_image, cond_image, ref_prompt, style_prompt, target_prompt
-    #     # )
+        # composite on full image
+        final_image_resized = cv2.resize(final_image, (w_crop, h_crop))
+        asset_image_resized = cv2.resize(asset_image, (w_crop, h_crop))
+        img_composited = img_full.copy()
+        img_composited[x_min:x_max, y_min:y_max] = final_image_resized
+        asset_image_full = np.zeros((img_full.shape[0],img_full.shape[1],4)).astype('uint8')
+        asset_image_full[x_min:x_max, y_min:y_max] = asset_image_resized
 
-    #     generated = np.array(generated)
-    #     # blur to match resolution of original image
-    #     generated = cv2.blur(generated, (5, 5))
-        
-    #     # include every part in final binary mask
-    #     deformed_mask = (deformed_mask>0) | (teeth>0) | (tongue>0)
-    #     deformed_mask_im = None
-    #     predict_generated_mask = False
-    #     pred_segmap = None
-    #     if predict_generated_mask:
-    #         pred_segmap, pred_labels = sam_face_model.predict(generated, roi_cropped)
-    #         deformed_mask = (pred_labels==2) | (pred_labels==7) | (pred_labels==10)
-    #         deformed_mask_im = refiner.refine(generated, deformed_mask.astype('uint8')*255, fast=False, L=900)
-    #     else:
-    #         deformed_mask_im = cv2.dilate(deformed_mask.astype('uint8')*255, (23,23))
-    #         deformed_mask_im = refiner.refine(generated, deformed_mask_im, fast=False, L=900)
-    #     deformed_mask_im = np.repeat(deformed_mask_im[..., np.newaxis], 3, axis=2)
-    #     deformed_mask_im = cv2.blur(deformed_mask_im, (23, 23))
-    #     deformed_mask_im = deformed_mask_im.astype("float32")/255
-    #     final_image = base_image_np * (1 - deformed_mask_im) + generated * deformed_mask_im
-    #     final_image = final_image.astype("uint8")
-    #     asset_image = np.concatenate([generated,255*deformed_mask_im[:,:,0:1]],-1).astype('uint8')
+        asset_image_cropped = asset_image_full[x_min:x_max, y_min:y_max]
+        img_composited_cropped = img_composited[x_min:x_max, y_min:y_max]
+        asset_image_cropped = cv2.resize(asset_image_cropped, (1024, 1024))
+        img_composited_cropped = cv2.resize(img_composited_cropped, (1024, 1024))
 
+        asset_image_final = cv2.resize(asset_image_full, (h_orig, w_orig))
+        img_composited_final = cv2.resize(img_composited, (h_orig, w_orig))
 
-    #     # composite on full image
-    #     final_image_resized = cv2.resize(final_image, (w_crop, h_crop))
-    #     asset_image_resized = cv2.resize(asset_image, (w_crop, h_crop))
-    #     img_composited = img_full.copy()
-    #     img_composited[x_min:x_max, y_min:y_max] = final_image_resized
-    #     asset_image_final = np.zeros((img_full.shape[0],img_full.shape[1],4)).astype('uint8')
-    #     asset_image_final[x_min:x_max, y_min:y_max] = asset_image_resized
-
-    #     # face cropping window
-    #     face_padding = 20
-    #     face_Xs, face_Ys = np.where(roi)
-    #     face_x_min, face_x_max = np.min(face_Xs) - face_padding, np.max(face_Xs) + face_padding
-    #     face_y_min, face_y_max = np.min(face_Ys) - face_padding, np.max(face_Ys) + face_padding
-    #     if face_x_min < 0:
-    #         face_x_min = 0
-    #     if face_y_min < 0:
-    #         face_y_min = 0
-    #     if face_x_max > 1024:
-    #         face_x_max = 1024
-    #     if face_y_max > 1024:
-    #         face_y_max = 1024
-
-    #     img_face = img_full[face_x_min:face_x_max, face_y_min:face_y_max]
-    #     img_composited_face = img_composited[face_x_min:face_x_max, face_y_min:face_y_max]
-    #     asset_image_face = asset_image_final[face_x_min:face_x_max, face_y_min:face_y_max]
-    #     img_face = cv2.resize(img_face, (1024, 1024))
-    #     img_composited_face = cv2.resize(img_composited_face, (1024, 1024))
-    #     asset_image_face = cv2.resize(asset_image_face, (1024, 1024))
-
-    #     # plot images
-    #     TITLE_SIZE = 35
-    #     fig, ax = plt.subplots(1,5, figsize=(50,10))
-    #     ax[0].imshow(img_face)
-    #     ax[0].set_title("Original Image", fontsize=TITLE_SIZE)
-    #     ax[0].axis('off')
-    #     ax[1].imshow(ref_image)
-    #     ax[1].set_title("Reference Style Image (Cropped)", fontsize=TITLE_SIZE)
-    #     ax[1].axis('off')
-    #     ax[2].imshow(cond_image)
-    #     ax[2].set_title("Modified Segmap (Mouth)", fontsize=TITLE_SIZE)
-    #     ax[2].axis('off')
-    #     ax[3].imshow(generated)
-    #     ax[3].set_title("Generated", fontsize=TITLE_SIZE)
-    #     ax[3].axis('off')
-    #     # ax[4].imshow(pred_segmap)
-    #     # ax[4].set_title("New Segmap", fontsize=TITLE_SIZE)
-    #     # ax[4].axis('off')
-    #     ax[4].imshow(img_composited_face)
-    #     ax[4].set_title("Composited", fontsize=TITLE_SIZE)
-    #     ax[4].axis('off')
-    #     plt.savefig( join(output_dir,f'{label_name.split("_")[0]}_{preset_class}_{shape_id}_plot.png') )
-    #     plt.close()
-    #     cv2.imwrite(join(output_dir,f'{label_name.split("_")[0]}_{preset_class}_{shape_id}_face.png'), cv2.cvtColor(img_composited_face, cv2.COLOR_RGB2BGR))
-    #     cv2.imwrite(join(output_dir,f'{label_name.split("_")[0]}_{preset_class}_{shape_id}_asset.png'), cv2.cvtColor(asset_image_face, cv2.COLOR_RGBA2BGRA))
-
-    #     # save images
-    #     # Image.fromarray(final_image).save(
-    #     #     join(
-    #     #         output_dir,
-    #     #         f'{label_name.split("_")[0]}_{preset_class}_pose{mouth_pose}_{shape_id}.png',
-    #     #     )
-    #     # )
-    #     # Image.fromarray(condgenerated_adjusted_image).save(
-    #     #     join(
-    #     #         output_dir,
-    #     #         f'{label_name.split("_")[0]}_{preset_class}_pose{mouth_pose}_{shape_id}_gen.png',
-    #     #     )
-    #     # )
+        # save assets
+        cv2.imwrite(f'{asset_outdir}/{preset_class}_{shape_id}.png', bgr_conversion(asset_image_final))
+        cv2.imwrite(f'{asset_outdir}/{preset_class}_{shape_id}_face.png', bgr_conversion(asset_image_cropped))
+        # save composited images
+        cv2.imwrite(f'{composite_outdir}/{preset_class}_{shape_id}.png', bgr_conversion(img_composited_final))
+        cv2.imwrite(f'{composite_outdir}/{preset_class}_{shape_id}_face.png', bgr_conversion(img_composited_cropped))
